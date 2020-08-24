00d7ccbf11ddfc77f205ba88fa2a2f16f4795094
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-24 08:21:42 -0300

    mudanças

--- Python/Outras Fontes/Teoria/3 - tipos primitivos.py
+++ Python/Outras Fontes/Teoria/3 - tipos primitivos.py
@@ -1,9 +1,21 @@
-print('Sem especificar o tipo primitivo ')
+print('Sem especificar o tipo primitivo:str ')
 n1 = input('Digite aqui um número inteiro: ')
 print(type(n1))
 print('apesar de {} ser da classe "numérica;int", o computador não entende isso e ele irá interpretar por padrão stringer. Assim, é necessário dizer para ele em qual tipo primitivo a sua variável se enquadrará.'.format(n1))
 
-print('Especificando o tipo primitivo ')
-n2 = int(input('Digite aqui um número: '))
-print(type(n1))
+print('Especificando o tipo primitivo:int ')
+n2 = int(input('Digite aqui um número Inteiro: '))
+print(type(n2))
 print('Agora especificando, o computador reconhece {} como "int".'.format(n2))
+
+print('Especificando o tipo primitivo:float')
+n3 = float(input('Digite aqui um valor real:'))
+print(type(n3))
+
+print('Especificando o tipo primitivo:str')
+n4 = str(input('Digite aqui um caracter:'))
+print(type(n4))
+
+print('Especificando o tipo primitivo:bool')
+n5 = bool(input('digita qualquer coisa:'))
+print(type(n5))
---
+++ Python/Outras Fontes/Teoria/4 - Saída de Dados.py
@@ -0,0 +1,14 @@
+n1 = bool(input('digite algo(testando bool): '))
+print(n1)
+
+n = input('Digite algo (testando isnumeric - númerico?):')
+print(n.isnumeric()) #isnumeric diz se é possível converter o valor em um número ou um tipo primitivo antes dele
+
+n = input('Digite algo (testando isalpha - alfabético?):')
+print(n.isalpha())
+
+n = input('Digite algo (testando isalnum - alfabético numerico?):')
+print(n.isalnum())
+
+n = input('Digite algo (testando isupper - letras maiúsculas?):')
+print(n.isupper())
