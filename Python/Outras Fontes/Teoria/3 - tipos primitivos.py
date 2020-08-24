e2e4618190c4a77cdcc8e3207d54d4d8ff44d173
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-24 07:28:23 -0300

    Tipo primitivo, observação
    stringer por padrão

---
+++ Python/Outras Fontes/Teoria/3 - tipos primitivos.py
@@ -0,0 +1,4 @@
+n1 = input('Digite aqui um número inteiro: ')
+print(type(n1))
+print('apesar de {} ser da classe "numérica;int", o computador não entende isso e ele irá interpretar por padrão stringer. Assim, é necessário dizer para ele em qual tipo primitivo a sua variável se enquadrará.'.format(n1))
+
