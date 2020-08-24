c00f9109cb2c90045ec94de6880f591dcdf5bd6c
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-24 09:13:15 -0300

    EDIT

--- Python/Outras Fontes/Desafios/4.1 - desafio.py
+++ Python/Outras Fontes/Desafios/4.1 - desafio.py
@@ -1,9 +1,7 @@
-#Desafio 004: Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
-
 n = input('Digita algo ai, fazendo favor: ') 
-print('O tipo primitivo dele é:', type(n))
-print('É um número?:', n.isnumeric())
-print('É uma palavra ou letra?:', n.isalpha())
+print(f'O tipo primitivo dele é:{type(n)}'
+print('É um número?:{n.isnumeric()}'
+print('É uma palavra ou letra?: {n.isalpha()}' 
 print('Possui só letras minúsculas?', n.islower())
 print('Possui só letras maiúsculas?', n.isupper())
 print('Seria um número alfanumérico?', n.isalnum())
