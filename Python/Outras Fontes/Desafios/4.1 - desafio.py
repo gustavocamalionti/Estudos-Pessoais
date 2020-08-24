445f492420a2b62cb827c072b06564f0d8dc222f
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-24 09:31:06 -0300

    update

--- Python/Outras Fontes/Desafios/4.1 - desafio.py
+++ Python/Outras Fontes/Desafios/4.1 - desafio.py
@@ -1,4 +1,4 @@
-#Desafio 4.1: Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele de outra forma, utilizando o conceito de {} visto na aula teórica de tipos primitivos.
+#Desafio 4.1: Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele de outra forma, utilizando o conceito de {} visto na aula teórica de tipos primitivos print.
 
 n = input('Digita algo ai, fazendo favor: ') 
 print(f'O tipo primitivo de {n} é: {type(n)}')
