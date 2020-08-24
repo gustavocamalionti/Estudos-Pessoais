53501c0519da954aa51181ef52a0ffd2e2bedaf3
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-24 07:14:55 -0300

    Curso/desafio

---
+++ Python/Outras Fontes/Desafios/3-desafio.py
@@ -0,0 +1,6 @@
+print('Desafio 03: Crie um Script Python que leia dois números e tente mostrar a soma entre eles.')
+
+a = int(input('número a: '))
+b = int(input('número b: '))
+s = a+b
+print('a soma entre "a" e "b" é:',s)
---
+++ Python/Outras Fontes/Teoria/2.1 - Exercício.py
@@ -0,0 +1,8 @@
+print('Exercício 2-1: Crie um Script Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada')
+
+name = input('Me diz ai, qual o seu nome? ')
+dia = input('Que dia você nasceu? ')
+mes = input('Hmmm interessante, agora preciso do mês de nascimento, por extenso por favor. ')
+ano = input('Por último, em que ano você veio ao mundo? ')
+
+print('Muito obrigado,',name+'! Sua inscrição foi concluída e você nasceu no dia',dia,'do mês de',mes,'do ano de',ano+'. Correto? ')
