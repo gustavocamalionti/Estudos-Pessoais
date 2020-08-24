c6f3ae7d3841c52daa9d93d2f243e64c9bd36be1
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-24 08:56:20 -0300

    update desafio 4
    colocando mais testes para a mesma variável

--- Python/Outras Fontes/Desafios/4 - desafio.py
+++ Python/Outras Fontes/Desafios/4 - desafio.py
@@ -1,6 +1,10 @@
 #Desafio 004: Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
 
 n = input('Digita algo ai, fazendo favor: ')
-print('é um número?:', n.isnumeric())
-print('é uma palavra ou letra?:', n.isalpha())
-print('Possui só letras maiúsculas?', n.islower())
+print('O tipo primitivo dele é:', type(n))
+print('É um número?:', n.isnumeric())
+print('É uma palavra ou letra?:', n.isalpha())
+print('Possui só letras minúsculas?', n.islower())
+print('Possui só letras maiúsculas?', n.isupper())
+print('Seria um número alfanumérico?', n.isalnum())
+print('só existe espaços?', n.isspace())
