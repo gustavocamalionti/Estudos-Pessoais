efb9bfd7697adb63d18b43fcd9d3dba93ab17c5f
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-24 07:35:48 -0300

    int

--- Python/Outras Fontes/Teoria/3 - tipos primitivos.py
+++ Python/Outras Fontes/Teoria/3 - tipos primitivos.py
@@ -1,4 +1,9 @@
+print('Sem especificar o tipo primitivo ')
 n1 = input('Digite aqui um número inteiro: ')
 print(type(n1))
 print('apesar de {} ser da classe "numérica;int", o computador não entende isso e ele irá interpretar por padrão stringer. Assim, é necessário dizer para ele em qual tipo primitivo a sua variável se enquadrará.'.format(n1))
 
+print('Especificando o tipo primitivo ')
+n2 = int(input('Digite aqui um número: '))
+print(type(n1))
+print('Agora especificando, o computador reconhece {} como "int".'.format(n2))
