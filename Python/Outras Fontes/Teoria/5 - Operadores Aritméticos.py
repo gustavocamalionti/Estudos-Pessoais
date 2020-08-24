fcc2ce6f723ec4da5d2f7fc81cf2ccbfe6462ef6
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-24 10:27:34 -0300

    descrição das op

--- Python/Outras Fontes/Teoria/5 - Operadores Aritméticos.py
+++ Python/Outras Fontes/Teoria/5 - Operadores Aritméticos.py
@@ -1,7 +1,7 @@
 print(5+2) #Soma
 print(5-2) #Subtração
-print(5*2)
-print(5/2)
-print(5**2)
-print(5//2)
-print(5%2)
+print(5*2) #Multiplicação
+print(5/2) #Divisão
+print(5**2) #Potênciação
+print(5//2) #Divisão Inteira
+print(5%2) #Resto da Divisão
