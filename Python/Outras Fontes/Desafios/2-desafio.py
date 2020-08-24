5207fa0e49045893f826c1c1f4e1352d52b35192
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-24 06:34:21 -0300

    desafio 2 informação extra
    Modo 2 de ser feito a mesma coisa

---
+++ Python/Outras Fontes/Desafios/2-desafio.py
@@ -0,0 +1,9 @@
+print('Desafio 002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas. ')
+
+print('Modo:01')
+nome = input('Digite seu nome, por favor. ')
+print('muito prazer,',nome+'!')
+
+print('Modo:02')
+nome = input('Digite seu nome, por favor. ')
+print('muito prazer, {}!'.format(nome))
