12e07876c96adf00ec4abb0be17b7d6a6230de71
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-25 08:25:00 -0300

    edit

--- Python/Outras Fontes/Desafios/16 - desafio.py
+++ Python/Outras Fontes/Desafios/16 - desafio.py
@@ -1,1 +1,2 @@
-#Desafio 016: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira. Ex: Digite um número: 6.127. O número 6.127 tem a mesma parte inteira 6.
+#Desafio 016: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira. 
+# Ex: Digite um número: 6.127. O número 6.127 tem a mesma parte inteira 6.
