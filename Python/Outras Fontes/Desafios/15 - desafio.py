0e9f064e9850a61f882a8ca6f416eafa57cf485d
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-25 12:39:20 -0300

    edit

--- Python/Outras Fontes/Desafios/15 - desafio.py
+++ Python/Outras Fontes/Desafios/15 - desafio.py
@@ -1,1 +1,7 @@
 #Desafio 015: escreva um programa que pergunte a quantidade de km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0.15 por km rodado.
+
+from math import ceil
+n1 = float(input('Quantos dias você ficou com o carro? '))
+n2 = float(input(f'Ok. já que você ficou {ceil(n1)} dias com ele, andou quantos km? '))
+v = n1*60 + n2*0.15
+print(f'Legal. Então você precisará pagar R${v:.2f} pelo aluguel. ')
--- Python/Outras Fontes/Desafios/20 - desafio.py
+++ Python/Outras Fontes/Desafios/20 - desafio.py
@@ -1,1 +1,1 @@
-#Desafio 020: O mesmo professor do desafio anterior quer sortear a ordem de apresentação de tarbalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem deles
+#Desafio 020: O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem deles
