1668cd69e9408d715ef93a89ac93cf6250e64756
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-25 00:02:26 -0300

    teoria modulos

---
+++ Python/Outras Fontes/Teoria/8 - Utilizando Módulos.py
@@ -0,0 +1,13 @@
+#Módulo Geral
+import math
+n = int(input('Digite um número: '))
+raiz = math.sqrt(n)
+print(f'A raiz de {n} é {raiz:.3f}. ')
+
+#Módulo Específico
+from math import sqrt
+n = int(input('Digite um número: '))
+print(f'A raiz de {n} é {sqrt(n):.3f}. ')
+
+#Importando uma biblioteca da comunidade
+import emoji
