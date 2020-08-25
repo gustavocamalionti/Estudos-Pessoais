ebf8f90b67b01acaecf259e3c147e1550d934892
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-24 21:49:55 -0300

    Edit

--- Python/Outras Fontes/Desafios/5 - desafio.py
+++ Python/Outras Fontes/Desafios/5 - desafio.py
@@ -1,7 +1,7 @@
 #Desafio 005: Faça um programa que leia um número inteiro e mostre o seu antecessor e seu sucessor.
 
-n1 = int(input('Digite um número inteiro: '))
-s = n1+1
-a = n1-1
+n = int(input('Digite um número inteiro: '))
+s = n+1
+a = n-1
 print(f'Logo, seu sucessor é {s} e seu antecessor é {a}. ')
-#print(f'Logo, seu sucessor é {n1+1} e seu antecessor é {n1-1}. ')
+#print(f'Logo, seu sucessor é {n+1} e seu antecessor é {n-1}. ')
