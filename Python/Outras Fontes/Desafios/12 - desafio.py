5bffb3d1a1d901f5a6f7834b1108a1aeeb5ac046
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-24 21:35:35 -0300

    Desafio 12 feito - Descontos aplicados e valor real

--- Python/Outras Fontes/Desafios/12 - desafio.py
+++ Python/Outras Fontes/Desafios/12 - desafio.py
@@ -1,1 +1,6 @@
 #Desafio 012: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+p1 = float(input('Qual o preço do produto, sem desconto? '))
+d = p1*0.05
+vr = p1-d
+print(f'Sem desconto ele vale R${p1:.2f} reais. Se aplicarmos 0.05x de desconto, teremos R${vr} reais.')
