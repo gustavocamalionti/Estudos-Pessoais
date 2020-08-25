62ea02963635b5855719479a82f068a18f32b7b4
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-25 01:29:11 -0300

    usando modulos do pypi

--- Python/Outras Fontes/Teoria/8 - Utilizando Módulos.py
+++ Python/Outras Fontes/Teoria/8 - Utilizando Módulos.py
@@ -9,6 +9,7 @@ from math import sqrt
 n = int(input('Digite um número: '))
 print(f'A raiz de {n} é {sqrt(n):.3f}. ')
 
-#Importando uma biblioteca da comunidade
-import emoji
+#Importando uma biblioteca da comunidade: Para importar uma biblioteca do pypi, necessita usar no terminal o comando(pip install "E o nome do pacote, sem as aspas")
 
+import emoji
+print(emoji.emojize('Python é legal :thumbsup:', use_aliases = True))
