25fda58afc6c8eb2e0ca33c75ce4b6d8ef5b0ddf
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-24 21:38:15 -0300

    update dolar

--- Python/Outras Fontes/Desafios/10 - desafio.py
+++ Python/Outras Fontes/Desafios/10 - desafio.py
@@ -1,6 +1,6 @@
-#Desafio 010: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Considere US$1,00=3,27
+#Desafio 010: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Considere US$1.00=5.61
 
 money = float(input('Quantos reais você tem na carteira? '))
-d = money/3.27
-print(f'Com R${money:.3f} reais você conseguiria comprar, tendo em vista que o dolar vale R$3.27 reais, US${d:.3f}. ')
-#print(f'Com R${money:3f} reais voce conseguiria comprar, tendo em vista que o dolar vale R$3.27 reais, US${money/3.27:.3f} ')
+d = money/5.61
+print(f'Com R${money:.3f} reais você conseguiria comprar, tendo em vista que o dolar vale R$5.61 reais, um total de US${d:.3f}. Que tristeza. ')
+#print(f'Com R${money:3f} reais voce conseguiria comprar, tendo em vista que o dolar vale R$5.61 reais, US${money/3.27:.3f} ')
