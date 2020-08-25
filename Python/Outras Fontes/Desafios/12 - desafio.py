12e6d71d5394bc65c17768feebff4cc4aaf3fff5
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-24 22:06:29 -0300

    edit

--- Python/Outras Fontes/Desafios/12 - desafio.py
+++ Python/Outras Fontes/Desafios/12 - desafio.py
@@ -1,6 +1,5 @@
 #Desafio 012: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
 p1 = float(input('Qual o preço do produto, sem desconto? '))
-d = p1*0.05
-vr = p1-d
-print(f'Sem desconto ele vale R${p1:.2f} reais. Se aplicarmos 0.05x de desconto, teremos R${vr} reais. ')
+d = p1 - (p1*0.05)
+print(f'Sem desconto ele vale R${p1:.2f} reais. Na promoção com 0.05% de desconto, ele vai custar R${d:.2f} reais. ')
