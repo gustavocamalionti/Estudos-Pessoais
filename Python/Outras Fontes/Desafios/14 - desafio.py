9007cf4bd2afa800918b43caee25e185d68fdf8c
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-24 22:36:01 -0300

    melhorei a equação

--- Python/Outras Fontes/Desafios/14 - desafio.py
+++ Python/Outras Fontes/Desafios/14 - desafio.py
@@ -1,5 +1,5 @@
 #Desafio 014: Escreva um programa que converta uma temperatura digitada em °C e converta para °F.
 
 c = float(input('Temperatura em Graus °C: '))
-f = (c*(9/5))+32 
+f = ((c*9)/5)+32 
 print(f'Se em Graus é {c}°C, então em Fahrenheit vale {f}°F ')
