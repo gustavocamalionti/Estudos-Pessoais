335e67e35844dcf7e5b94f893da0d30a2880c15a
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-25 16:41:53 -0300

    desafio

--- Python/Outras Fontes/Desafios/19 - desafio.py
+++ Python/Outras Fontes/Desafios/19 - desafio.py
@@ -1,1 +1,11 @@
 #Desafio 019: Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+
+import random
+
+a = str(input('Digite o nome do aluno A: '))
+b = str(input('Digite o nome do aluno B: '))
+c = str(input('Digite o nome do aluno C: '))
+d = str(input('Digite o nome do aluno D: '))
+lista = [a, b, c, d]
+escolhido = random.choice(lista)
+print(f'Sorteando um aluno dos 4 da sala, o {escolhido} foi escolhido! ')
--- Python/Outras Fontes/Desafios/20 - desafio.py
+++ Python/Outras Fontes/Desafios/20 - desafio.py
@@ -1,1 +1,14 @@
 #Desafio 020: O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem deles
+
+import random
+
+a = str(input('Digite o nome do primeiro representante: '))
+b = str(input('Digite o nome do segundo representanto: '))
+c = str(input('Digite o nome do terceiro representante: '))
+d = str(input('Digite o nome do Quarto representante:'))
+
+lista = [a, b, c, d]
+emb = random.sample(lista, 4)
+
+
+print(f'A sequência de apresentação dos grupos será: {emb}')
