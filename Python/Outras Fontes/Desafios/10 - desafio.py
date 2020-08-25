73bbf80b7264b15f14580de952ab41b735fed03e
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-24 21:19:49 -0300

    Desafio 10 feito - Conversor de R$ para US$

--- Python/Outras Fontes/Desafios/10 - desafio.py
+++ Python/Outras Fontes/Desafios/10 - desafio.py
@@ -1,1 +1,6 @@
 #Desafio 010: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Considere US$1,00=3,27
+
+money = float(input('Quantos reais você tem na carteira? '))
+d = money/3.27
+print(f'Com R${money:.3f} reais você conseguiria comprar, tendo em vista que o dolar vale R$3.27 reais, US${d:.3f}. ')
+#print(f'Com R${money:3f} reais voce conseguiria comprar, tendo em vista que o dolar vale R$3.27 reais, US${money/3.27:.3f} ')
