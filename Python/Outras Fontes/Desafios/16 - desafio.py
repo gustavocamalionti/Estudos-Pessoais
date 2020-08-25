01eeb83e5d3574227c8144783332445c02e07ff2
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-25 10:38:40 -0300

    Desafio 16 feito - parte inteira

--- Python/Outras Fontes/Desafios/16 - desafio.py
+++ Python/Outras Fontes/Desafios/16 - desafio.py
@@ -1,2 +1,7 @@
 #Desafio 016: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira. 
 # Ex: Digite um número: 6.127. O número 6.127 tem a mesma parte inteira 6.
+
+from math import trunc 
+
+n = float(input('Digite um número: '))
+print(f'O número {n} tem a parte inteira {trunc(n)} ')
