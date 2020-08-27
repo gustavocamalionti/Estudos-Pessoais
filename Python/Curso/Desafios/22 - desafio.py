a811a90b70de7f9a2eadd2083bce671477cb878f
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-27 12:07:01 -0300

    Desafio feito

--- Python/Curso/Desafios/22 - desafio.py
+++ Python/Curso/Desafios/22 - desafio.py
@@ -3,3 +3,19 @@
  #O nome com todas minúsculas. 
  #Quantas letras ao todo (sem considerar espaços). 
  #Quantas letras tem o primeiro nome.
+
+
+#MODO 1: Feito sozinho, sem ajuda de gabarito.
+name = str(input('Digite o seu nome: '))
+print('Analisando...')
+
+letras = len(name)
+espacos = name.count(' ')
+lista = name.split()
+
+print(f'seu nome em maíusculo: {name.upper()}. ')
+print(f'Seu nome em minúsculo: {name.lower()}. ')
+print(f'Há {letras - espacos} letras em seu nome. ')
+print(f'Seu nome inicial é {lista[0]} e ele possui {len(lista[0])} letras.')
+
+#Modo 2: Resolução do Professor.
