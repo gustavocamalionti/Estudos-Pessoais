f953eccbcce085c6574fcc81186cea3764dd211c
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-27 13:29:40 -0300

    resolução do professor
    encontrei um furo no código

--- Python/Curso/Desafios/22 - desafio.py
+++ Python/Curso/Desafios/22 - desafio.py
@@ -7,15 +7,24 @@
 
 #MODO 1: Feito sozinho, sem ajuda de gabarito.
 name = str(input('Digite o seu nome: '))
-print('Analisando...')
 
 letras = len(name)
 espacos = name.count(' ')
 lista = name.split()
 
-print(f'seu nome em maíusculo: {name.upper()}. ')
+print('Analisando...')
+print(f'seu nome em maiúsculo: {name.upper()}. ')
 print(f'Seu nome em minúsculo: {name.lower()}. ')
 print(f'Há {letras - espacos} letras em seu nome. ')
 print(f'Seu nome inicial é {lista[0]} e ele possui {len(lista[0])} letras.')
 
-#Modo 2: Resolução do Professor.
+#Modo 2: Resolução do Professor.(esse modo só funciona bem se a pessoa digitasse o nome completo dela, caso fosse apenas o primeiro nome, daria erro! A minha resolução considera todos os casos.)
+name = str(input('Digite seu nome aqui: ')).strip()
+
+print('Analisando...')
+print(f'Seu nome em maiúsculo é {name.upper()}. ')
+print(f'Seu nome em minúsculo é {name.lower()}. ')
+print('Há {} letras em seu nome. '.format(len(name) - name.count(' ')))
+print('Seu primeiro nome tem {} letras em seu nome.'.format(name.find(' ')))
+
+
