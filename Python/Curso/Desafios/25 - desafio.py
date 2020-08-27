706416f62081903e611faa920a621ffc64da6e8b
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-27 09:53:46 -0300

    edit - criação dos desafios

---
+++ Python/Curso/Desafios/25 - desafio.py
@@ -0,0 +1,1 @@
+#Desafio 024: Crie um programa que leia o nome de uma pessoa e diga se ela tem "Silva" no nome.
---
+++ Python/Curso/Desafios/26 - desafio.py
@@ -0,0 +1,4 @@
+#Desafio 024: Faça um programa que leia uma frase pelo teclado e mostre: 
+# Quantas vezes aparece a letra "a". 
+# Em que posição ela aparece a primeira vez. 
+# Em que posição ela aparece a última vez.
