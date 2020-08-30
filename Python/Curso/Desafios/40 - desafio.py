3808b14344df9ae74db294df1be52d5a4b2a8738
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-30 11:15:20 -0300

    Desafio completo

--- Python/Curso/Desafios/40 - desafio.py
+++ Python/Curso/Desafios/40 - desafio.py
@@ -2,3 +2,20 @@
 #  - Média abaixo de 5.0? REPROVADO.
 # - Média entre 5.0 e 6.9:RECUPERAÇÃO.
 #  - Média 7.0 ou superior: APROVADO.
+
+a = float(input('Nota na P1: '))
+b = float(input('Nota na P2: '))
+
+#VALIDAÇÃO DAS NOTAS
+if a>10 or a<0 or b>10 or b<0:
+    print(f'\033[37;41mNOTA INVALIDA!\033[m O sistema só aceitará valores entre 0 e 10. ')
+    exit()
+
+#CALCULO DAS MÉDIAS
+media = (a+b)/2
+if media < 5.0: 
+    print(f'\033[37;41mREPROVADO!\033[m Sua média foi de \033[31m{media}\033[m. ')
+if media >= 5 and media < 7: #7>media>=5
+    print(f'\033[37;44mRECUPERAÇÃO!\033[m Sua média foi de \033[34m{media}\033[m.')
+if media >= 7: #else: também funcionaria!
+    print(f'\033[37;42mAPROVADO!\033[m Sua média foi de \033[32m{media}\033[m.')
