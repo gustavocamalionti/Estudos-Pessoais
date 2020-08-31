9820d80c2bb29d593fd765316a6e2f3404da59dc
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-31 17:01:59 -0300

    editei linha do código

--- Python/Curso/Desafios/44N - (Gerenciador de Pagamentos).py
+++ Python/Curso/Desafios/44N - (Gerenciador de Pagamentos).py
@@ -28,7 +28,10 @@ elif escolha == 3:
         print(f'OPÇÃO INVÁLIDA! TENTE NOVAMENTE.')
 elif escolha == 4:
     parcela = int(input('Quantas Parcelas? '))
-    print(f'''A sua compra será parcelada em {parcela:.0f}x de R${((valor+valor*0.2)/parcela):.2f} no cartão, COM JUROS. 
-    Sua compra de R${valor:.2f} vai custar \033[37;41mR${(valor+valor*0.2):.2f}\033[m no final. ''')
+    if parcela>=3:
+        print(f'''A sua compra será parcelada em {parcela:.0f}x de R${((valor+valor*0.2)/parcela):.2f} no cartão, COM JUROS.
+        Sua compra de R${valor:.2f} vai custar \033[37;41mR${(valor+valor*0.2):.2f}\033[m no final. ''')
+    else:
+        print('OPÇÃO INVÁLIDA! TENTE NOVAMENTE.')
 else:
     print('OPÇÃO INVÁLIDA! TENTE NOVAMENTE.')
