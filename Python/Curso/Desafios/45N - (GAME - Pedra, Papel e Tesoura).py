bc4e463ea1b9cf9d4601fc52622456418ceb5b79
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-31 20:46:32 -0300

    Resolução pessoal - CONSEGUI!!!!

--- Python/Curso/Desafios/45N - (GAME - Pedra, Papel e Tesoura).py
+++ Python/Curso/Desafios/45N - (GAME - Pedra, Papel e Tesoura).py
@@ -3,6 +3,7 @@
 import random
 from time import sleep
 
+#RESOLUÇÃO PESSOAL
 print('-=-'*30)
 print('''Escolha uma opção:
 [ 0 ] - PEDRA
@@ -27,13 +28,17 @@ sleep(1)
 print('PAPEL...')
 sleep(1)
 print('TESOURA!')
-print('-=-'*30)  
+print('-=-'*30) 
+sleep(0.5) 
 
 lista1 = 'PEDRA PAPEL TESOURA'
 l = lista1.split()
 
 escolhapc = random.sample(l, 1)
 print(f'Jogador escolheu {jog}. \nComputador escolheu {escolhapc[0]}. ')
-if (jog == 'PEDRA' and escolhapc == 'PEDRA') or (jog == 'TESOURA' and escolhapc == 'TESOURA') or (jog == 'PAPEL' and escolhapc == 'PAPEL'):
-    print('EMPATE')
-
+if jog == escolhapc[0]:
+    print('\033[37mEMPATE!\033[m')
+elif (jog == 'PEDRA' and escolhapc[0] == 'TESOURA') or (jog == 'TESOURA' and escolhapc[0] == 'PAPEL') or (jog == 'PAPEL' and escolhapc[0] == 'PEDRA'):
+    print('\033[32mVENCEU!\033[m')
+else:
+    print('\033[31mDERROTA!\033[m')
