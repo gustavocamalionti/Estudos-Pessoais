4cf7f367429e052a05cc9efe6f832b36362e2811
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-08-31 22:11:40 -0300

    Obs

---
+++ Python/Curso/Desafios/45V2 - (GAME - PEDRA PAPEL OU TESOURA).py
@@ -0,0 +1,31 @@
+#RESOLUÇÃO DO PROFESSOR:
+from time import sleep
+import random
+print('''Escolha uma opção:
+[ 0 ] - PEDRA
+[ 1 ] - PAPEL
+[ 2 ] - TESOURA ''')
+print('-=-'*30)
+
+jog = int(input('Qual é a sua jogada? '))
+
+sleep(0.5)
+print('PEDRA...')
+sleep(1)
+print('PAPEL...')
+sleep(1)
+print('TESOURA!')
+print('-=-'*30) 
+sleep(0.5) 
+
+itens = ['PEDRA', 'PAPEL', 'TESOURA']
+pc = random.randint(0,2)
+
+print(f'Jogador escolheu {itens[jog]}. \nComputador escolheu {itens[pc]}. ')
+if jog==pc:
+    print('EMPATE!')
+elif (jog<2 and pc==jog+1) or (pc==0 and jog==2):
+    print('DERROTA!')
+else:
+    print('VITÓRIA!')
+print('-=-'*30)
