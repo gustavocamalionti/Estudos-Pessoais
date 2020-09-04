60adcb7b572132ca72675d98be08782c2d75077d
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-09-03 22:49:44 -0300

    Desafio feito

--- Python/Curso/Desafios/48 - (Soma Ímpares Múltiplos de 3).py
+++ Python/Curso/Desafios/48 - (Soma Ímpares Múltiplos de 3).py
@@ -1,9 +1,10 @@
 #Desafio 048: Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de três e que se encontram no intervalo de 1 até 500.
 
-print('Soma de números impares e que são múltiplos de 3.')
-
-s = 0
+soma = 0
+cont = 0
 for c in range(1, 501, 2):
-    if c==1 or c%3==0:
-        s+=c
-        print(s)
+    if c%3==0:
+        soma = soma + c
+        cont = cont + 1
+
+print(f'A soma de todos os {cont} números vale {soma}')
--- Python/Curso/Desafios/50 - (Soma dos Pares).py
+++ Python/Curso/Desafios/50 - (Soma dos Pares).py
@@ -1,1 +1,9 @@
 #Desafio 050: Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+soma = 0
+cont = 0
+for c in range(1,7):
+    n = int(input('Digite um número: '))
+    if n%2==0:
+        soma = soma + n
+        cont = cont + 1        
+print(f'Você informou um total de {cont} números pares, portanto, a soma entre eles vale {soma}')
