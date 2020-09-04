55af1499f38b5c30fac572bc6fe6d206a3457eb9
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-09-04 19:05:34 -0300

    Desafio feito

--- Python/Curso/Desafios/53 - (Detector de Palíndromo).py
+++ Python/Curso/Desafios/53 - (Detector de Palíndromo).py
@@ -5,3 +5,18 @@
 #Ex: A Sacada da Casa 
 #ex: O lobo ama o bolo 
 #ex: anotaram a data da maratona
+
+frase = str(input('Digite uma frase ou nome qualquer: ')).strip().upper()
+lista = frase.split()
+junto =''.join(lista)
+inverso = ''
+
+
+for c in range(len(junto) -1, -1, -1):
+    inverso = inverso + junto[c]
+print(inverso)
+print(f' O inverso de {junto} é {inverso}.')
+if inverso == junto:
+    print(f' É uma palavra/frase Políndroma.')
+else:
+    print(f'Não é Políndroma')
--- Python/Curso/Desafios/54 - (Grupo da Maioridade).py
+++ Python/Curso/Desafios/54 - (Grupo da Maioridade).py
@@ -1,1 +1,16 @@
 #Desafio 054: Crie um programa que leia o ano de nascimento de sete pessoas. No final, moestre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+
+import datetime
+
+anoatual = datetime.date.today().year
+novo = 0
+velho = 0
+for c in range(1,8):
+    ano = int(input(f'Em que ano a {c}° pessoa nasceu? '))
+    if (anoatual - 18)<=ano<=anoatual:
+        novo = novo + 1
+    else:
+        velho = velho + 1
+print(f'Há {novo} pessoas menores de idade. ')
+print(f'Há {velho} pessoas maiores de idade. ')
+
