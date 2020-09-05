ad2d802ada33b337cd6788bb4791aa619fa3ed4a
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-09-04 23:23:38 -0300

    Desafio feito

--- Python/Curso/Desafios/54 - (Grupo da Maioridade).py
+++ Python/Curso/Desafios/54 - (Grupo da Maioridade).py
@@ -14,3 +14,12 @@ for c in range(1,8):
 print(f'Há {novo} pessoas menores de idade. ')
 print(f'Há {velho} pessoas maiores de idade. ')
 
+#OUTRO MÉTODO:
+print('-'*40)
+from datetime import date
+menores = 0
+for c in range(1, 8):
+    pessoa = int(input('Digite o ano de nascimento da {}ª pessoa: '.format(c)))
+    if date.today().year - pessoa < 21:
+        menores = menores + 1
+print('\n{} são Maiores e {} são menores.'.format(7 - menores, menores))
