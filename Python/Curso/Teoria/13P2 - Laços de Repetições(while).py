10c84a6f512cce91fbfd50b9475e46d1f5c4af43
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-09-09 11:36:32 -0300

    teoria

---
+++ Python/Curso/Teoria/13P2 - Laços de Repetições(while).py
@@ -0,0 +1,22 @@
+
+#for c in range(1,11):
+   # print(c)
+#print('FIM')
+
+#LIMITE DEFINIDO
+c = 1
+while c <= 10:
+    print(c)
+    c= c + 1 
+print('FIM')
+
+#LIMITE INDEFINIDO: 01
+n = 1
+while n!=0:
+    n = int(input('Digite um valor: '))
+print('FIM')
+
+r = 'S'
+while r == 'S':
+    n = int(input('Digite um valor: '))
+    r = str(input('Quer continuar? [S/N]')).upper()
