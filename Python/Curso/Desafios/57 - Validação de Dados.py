14b1ca089b2341ea23a4200c80352c85553f41cf
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-09-09 12:31:56 -0300

    edit

--- Python/Curso/Desafios/57 - Validação de Dados.py
+++ Python/Curso/Desafios/57 - Validação de Dados.py
@@ -1,1 +1,7 @@
 #Desafio 057: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.
+
+sexo2 = ''
+while sexo2 !='M' or sexo2 !='F':
+    sexo = str(input('Informe seu sexo: [M/F]? ')).strip().upper()
+    sexo2 = str(input('Dados inválidos. Por favor, informe seu sexo: ))
+print('Muito obrigado!')
