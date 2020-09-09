7fdc636352940c985eaeaba73c545858c10b0224
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-09-09 12:14:49 -0300

    Criei o arquivo de desafio e já o inclui

---
+++ Python/Curso/Desafios/61 - Progressão Aritmética v2.py
@@ -0,0 +1,1 @@
+#Desafio 061: Refaça o desafio 051, lendo o primeiro termo e a razão de uma pa, mostrando os 10 primeiros termos da progressão usando a estrutura while.
--- Python/Curso/Teoria/13P2 - Laços de Repetições(while).py
+++ Python/Curso/Teoria/13P2 - Laços de Repetições(while).py
@@ -16,7 +16,20 @@ while n!=0:
     n = int(input('Digite um valor: '))
 print('FIM')
 
+#LIMITE INDEFINIDO: 02
 r = 'S'
 while r == 'S':
     n = int(input('Digite um valor: '))
-    r = str(input('Quer continuar? [S/N]')).upper()
+    r = str(input('Quer continuar? [S/N]')).upper().strip()
+print('FIM')
+
+n = 1 
+par = impar = 0
+while n!=0:
+    n = int(input('Digite um valor: '))
+    if n!=0:
+        if n%2==0:
+            par = par + 1
+        else:
+            impar = impar + 1
+print(f'você digitou {par} números pares e {impar} números impares.')
