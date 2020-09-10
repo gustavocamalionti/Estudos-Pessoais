12c68579d0ae562a779ff3d60e356dc31023b483
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-09-09 21:35:04 -0300

    edit

--- Python/Curso/Desafios/58 - Jogo de Adivinhação V2.py
+++ Python/Curso/Desafios/58 - Jogo de Adivinhação V2.py
@@ -1,6 +1,5 @@
 #Desafio 058: Melhore o jogo do Desafio 028 onde o computador vai "pensar" em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
 
-
 import random 
 numeromaquina = random.randint(1,10)
 print('-'*50)
