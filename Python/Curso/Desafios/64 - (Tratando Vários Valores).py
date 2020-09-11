29d7bcf2c9653b63bcefb98a70698dafd655f2d8
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-09-11 15:20:33 -0300

    edit

--- Python/Curso/Desafios/64 - (Tratando Vários Valores).py
+++ Python/Curso/Desafios/64 - (Tratando Vários Valores).py
@@ -18,3 +18,13 @@ print(f'Você digitou {cont-1} números e a soma entre eles vale {soma-999}.')
 print('-'*30)
 print('MÉTODO 2')
 print('-'*30)
+
+n = 0
+cont = 0
+soma = 0
+n = int(input('Digite alguns valores inteiros [999 para parar]: '))
+while n!=999:
+    cont = cont + 1
+    soma = soma + n
+    n = int(input('Digite alguns valores inteiros [999 para parar]: '))
+print(f'Você digitou {cont} números e a soma entre eles vale {soma}.')
---
+++ Python/Curso/Teoria/13P3 - Laços de Rpeetições - interromper).py
@@ -0,0 +1,10 @@
+
+soma = quant = 0
+
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break 
+    quant = quant + 1
+    soma = soma + n
+print(f'Obrigado, você forneceu {quant} número e a soma entre eles vale {soma}')
