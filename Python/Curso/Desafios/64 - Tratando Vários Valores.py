e14c2979fadb6dd635cdc77c12d09917a44687a2
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-09-11 01:44:55 -0300

    obs - editar

--- Python/Curso/Desafios/64 - Tratando Vários Valores.py
+++ Python/Curso/Desafios/64 - Tratando Vários Valores.py
@@ -1,5 +1,10 @@
 #Desafio 064: Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles(desconsiderando o flag).
 
+#MÉTODO 1:
+print('-'*30)
+print('MÉTODO 1')
+print('-'*30)
+
 n = 0
 cont = 0
 soma = 0
@@ -9,3 +14,7 @@ while n!=999:
     soma = soma + n
 print(f'Você digitou {cont-1} números e a soma entre eles vale {soma-999}.')
 
+#MÉTODO 2:
+print('-'*30)
+print('MÉTODO 2')
+print('-'*30)
