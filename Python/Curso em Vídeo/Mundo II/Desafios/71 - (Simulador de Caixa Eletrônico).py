e3527c2a8c3fcae7fc4c4ef9353954ff2c9d14ac
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-09-14 18:20:03 -0300

    Exercício finalizado

--- Python/Curso em Vídeo/Mundo II/Desafios/71 - (Simulador de Caixa Eletrônico).py
+++ Python/Curso em Vídeo/Mundo II/Desafios/71 - (Simulador de Caixa Eletrônico).py
@@ -2,10 +2,32 @@
 #OBS: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1. 
 
 caixa = 'CAIXA ELETRÔNICO'
-print('-'*20)
-print(f'{caixa:^20}')
-print('-'*20)
+print('-'*37)
+print(f'{caixa:^37}')
+print('-'*37)
 
 
-saque = int(input('Qual será o valor de saque? '))
+saque = int(input('Qual será o valor de saque? R$'))
 
+total = saque 
+céd = 50
+totcéd = 0
+
+while True:
+    if total >= céd:
+        total = total - céd
+        totcéd = totcéd + 1
+    else:
+        if totcéd>0: 
+            print(f'Total de {totcéd} cédulas de R${céd:.2f}')
+        if céd == 50:
+            céd = 20
+        elif céd == 20:
+            céd = 10
+        elif céd == 10:
+            céd = 1
+        if total == 0:
+            break
+        totcéd = 0
+
+print('Volte sempre!!! Agradecemos a preferência.')
