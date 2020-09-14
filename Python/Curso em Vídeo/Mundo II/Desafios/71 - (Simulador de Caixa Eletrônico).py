e710a0690d66c21e34b07a42e4496b6bd909f8ac
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-09-14 14:49:26 -0300

    inicio do código

--- Python/Curso em Vídeo/Mundo II/Desafios/71 - (Simulador de Caixa Eletrônico).py
+++ Python/Curso em Vídeo/Mundo II/Desafios/71 - (Simulador de Caixa Eletrônico).py
@@ -1,2 +1,11 @@
 #Desafio 071: Crie um programa que simule o funcionamento de um caixa eletrônico. No inicio, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues. 
 #OBS: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1. 
+
+caixa = 'CAIXA ELETRÔNICO'
+print('-'*20)
+print(f'{caixa:^20}')
+print('-'*20)
+
+
+saque = int(input('Qual será o valor de saque? '))
+
