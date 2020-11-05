79189e7c4d294ffa7539158acae2d5f85db4b950
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-11-05 20:03:10 -0300

    Primeira tentativa, desafio completo
    Tive um entendimento de 75% referente o teste de posições, revisar!!!!

--- Python/Curso em Vídeo/Mundo III/Desafios/80.2 - (lista Ordenada Sem Repetições).py
+++ Python/Curso em Vídeo/Mundo III/Desafios/80.2 - (lista Ordenada Sem Repetições).py
@@ -1,22 +1,28 @@
 #Desafio 080: Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
 
 #RESOLUÇÃO PESSOAL - PRIMEIRA TENTATIVA
-
-primeirovalor = 0
-cont = 0
+pos = 0
 lista = []
-for c in range(5):
+for c in range(0, 5):
     n = int(input('Digite um número: '))
-    if cont == 0:
+    #situação 1: Primeiro Valor
+    if c == 0:
         lista.append(n)
+        print(f'Foi adicionado {n}, é o primeiro valor. ')
     
-    elif cont != 0:
-        for c in lista:
-            if min(lista)<n:
-                lista.append(n)
-                print(f' O valor {n} foi adicionado no final da lista')
-            
+    #situação 2: Maior valor
+    elif n > lista[len(lista)-1]: #elif n>lista[-1]:
+        lista.append(n)
+        print(f'Foi adicionado {n} no final da lista. ')
+    
+    #situação 3: valor intermediario
+    else:
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos, n)
+                break
 
-    cont = cont+ 1
+            pos = pos+1
+
 print(lista)
     
