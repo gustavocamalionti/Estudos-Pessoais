9ab30a9c2f15a17b844f8dbbc53aec8011223f39
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-11-05 15:41:58 -0300

    edit

---
+++ Python/Curso em Vídeo/Mundo III/Desafios/80.2 - (lista Ordenada Sem Repetições).py
@@ -0,0 +1,8 @@
+#Desafio 080: Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
+
+#RESOLUÇÃO PESSOAL - PRIMEIRA TENTATIVA
+
+for c in range(5):
+    n = int(input('Digite um número: '))
+
+    
