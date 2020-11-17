5d8b0a639b70c0bcc658c80ae9f451a66dd4b6a5
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-11-17 04:56:05 -0300

    Refiz o desafio - Entedimento de 90% do exercício

--- Python/Curso em Vídeo/Mundo III/Desafios/83 - (Validando Expressões Matemáticas).py
+++ Python/Curso em Vídeo/Mundo III/Desafios/83 - (Validando Expressões Matemáticas).py
@@ -1,22 +1,29 @@
 #Desafio 083: Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
 
-#RESOLUÇÃO PESSOAL: Não serve para todos os casos ))) 3*2 ((( expressão inválida
-contparent1=0
-contparent2=0
-expre = str(input(' Escreva uma expressão algébrica usando parênteses: '))
+#RESOLUÇÃO PESSOAL
+expressao_numerica = str(input('Digite uma expressão numérica com parênteses '))
 
-for c in range(0, len(expre)):
-    if expre[c]== '(':
-        contparent1 = contparent1 + 1
-    elif expre[c] == ')':
-        contparent2 = contparent2 + 1
+lista_parenteses = []
 
-if contparent1 == contparent2:
-    print('A expressão é válida')
+for c in range(0,len(expressao_numerica)): 
+    if expressao_numerica[c] == '(':
+        lista_parenteses.append('(')
+        print('Teste')
+    elif expressao_numerica[c] == ')':
+        if len(lista_parenteses) > 0:
+            lista_parenteses.pop()
+            print('teste')
+        elif len(lista_parenteses) == 0:
+            lista_parenteses.append(')')
+            break
+    
+if len(lista_parenteses) == 0:
+    print(f'A expressão é válida')
+    
 else:
-    print('A expressão é inválida')
-
-#RESOLUÇÃO DO PROFESSOR: Tentativa de fazer sozinho após ver a resolução
+    print(f'A expressão é inválida')
+        
+#RESOLUÇÃO DO PROFESSOR
 
 expre = str(input(' Escreva uma expressão algébrica usando parênteses: '))
 pilha = []
@@ -35,5 +42,7 @@ if len(pilha) == 0:
 else:
     print('Sua expressão está errada')
 
+
+        
     
     
