298af130663203f049a9c9ef7859c3a1260115b4
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-11-17 04:53:10 -0300

    Entendimento de 75% do exercício

--- Python/Curso em Vídeo/Mundo III/Desafios/80.2 - (lista Ordenada Sem Repetições).py
+++ Python/Curso em Vídeo/Mundo III/Desafios/80.2 - (lista Ordenada Sem Repetições).py
@@ -1,28 +1,32 @@
 #Desafio 080: Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
 
-#RESOLUÇÃO PESSOAL - PRIMEIRA TENTATIVA
+#RESOLUÇÃO PESSOAL : TERCEIRA TENTATIVA - Entendimento de 75% do exercício
 pos = 0
-lista = []
-for c in range(0, 5):
-    n = int(input('Digite um número: '))
-    #situação 1: Primeiro Valor
-    if c == 0:
-        lista.append(n)
-        print(f'Foi adicionado {n}, é o primeiro valor. ')
+lista_numeros = []
+
+for valor in range(1,6):
+    numeros = int(input(f'Digite o {valor}° valor: '))
     
-    #situação 2: Maior valor
-    elif n > lista[len(lista)-1]: #elif n>lista[-1]:
-        lista.append(n)
-        print(f'Foi adicionado {n} no final da lista. ')
+    if valor == 1: #primeiro
+        lista_numeros.append(numeros)
+        print(f'{numeros} é o primeiro valor.')
     
-    #situação 3: valor intermediario
+    elif numeros > max(lista_numeros): #maior
+        lista_numeros.append(numeros)
+        print(f'{numeros} foi adicionado no final da lista')
+    
     else:
-        while pos < len(lista):
-            if n <= lista[pos]:
-                lista.insert(pos, n)
+        while pos < len(lista_numeros):
+            if numeros <= lista_numeros[pos]:
+                lista_numeros.insert(pos, numeros)
+                print(f' O {numeros} foi adicionado na lista')
                 break
+            pos=pos+1
+            
+print(lista_numeros)
 
-            pos = pos+1
 
-print(lista)
+
     
+
+
