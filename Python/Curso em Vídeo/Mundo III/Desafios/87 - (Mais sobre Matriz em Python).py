509b9b387419b6dd2e46629beb550a1c5426cc09
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-11-17 06:22:32 -0300

    Criação do Enunciado

---
+++ Python/Curso em Vídeo/Mundo III/Desafios/87 - (Mais sobre Matriz em Python).py
@@ -0,0 +1,4 @@
+#Desafio 87: Aprimore o exercício anterior, mostrando no final: 
+# A) A soma de todos os valores pares digitados. 
+# B) A soma dos valores da terceira coluna. 
+# C) O maior valor da segunda linha
---
+++ Python/Curso em Vídeo/Mundo III/Desafios/88 - (Palpites para a Mega Sena).py
@@ -0,0 +1,1 @@
+#Desafio 88: Faça um programa que ajude um jogador da MEGA SENA a criar palpites. O programa vai perguntas quantos jogos serão gerados e vai sortear 6 números entre 1 e 60, para cada jogo, cadastrando tudo em uma lista composta.
