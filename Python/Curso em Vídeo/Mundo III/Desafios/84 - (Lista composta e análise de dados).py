4194ee5ff9509b72fff6293edce59acd7c0ec00c
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-11-24 11:03:54 -0300

    Resolução do Professor

--- Python/Curso em Vídeo/Mundo III/Desafios/84 - (Lista composta e análise de dados).py
+++ Python/Curso em Vídeo/Mundo III/Desafios/84 - (Lista composta e análise de dados).py
@@ -62,6 +62,45 @@ print('\nAs pessoas mais pesadas são:')
 for c in range(0,len(pessoas_peso[0])):
     print(pessoas_peso[0][c][0], end='...')
 
+#RESOLUÇÃO PROFESSOR:
+print('\n', '-=-'*25)
+dados = []
+pessoas_grupo = []
+pessoas_peso = [[], []]
+menu_resposta = ''
+peso_maior = peso_menor = 0
+contagem_pessoas = 0
 
+while True: 
+    dados.append(str(input('Digite seu nome: ')).upper().strip())
+    dados.append(float(input('Digite seu peso: ')))
+    if len(pessoas_grupo) == 0:
+        peso_maior = peso_menor = dados[1]
+    else:
+        if dados[1] > peso_maior:
+            peso_maior = dados[1]
+        elif dados[1] < peso_menor:
+            peso_menor = dados[1]
 
+    pessoas_grupo.append(dados[:])
+    dados.clear()
+
+    menu_resposta = str(input('Quer continuar? [S/N] ')).upper().strip()
+    while menu_resposta not in 'SN':
+        print('Resposta Inválida,',end='')
+        menu_resposta = str(input(' Tente novamente. Quer continuar? [S/N] ')).upper().strip()
+     
+    if menu_resposta == 'N':
+        break
+
+print(f'Ao todo, você cadastrou {len(pessoas_grupo)}')
+print(f'O maior peso foi de {peso_maior}. Peso de ', end='')
+for p in pessoas_grupo:
+    if p[1] == peso_maior:
+        print(f'{p[0]}', end='...')
+
+print(f'\nO menor peso foi de {peso_menor}. Peso de ', end='')
+for p in pessoas_grupo:
+    if p[1] == peso_menor:
+        print(f'{p[0]}', end='...')
 
