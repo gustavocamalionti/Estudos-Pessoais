a96bc040dd491ac56cada3b4262ef8d06f1a9342
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-11-24 12:28:08 -0300

    Edit

--- Python/Curso em Vídeo/Mundo III/Desafios/85 - (Listas com pares e ímpares).py
+++ Python/Curso em Vídeo/Mundo III/Desafios/85 - (Listas com pares e ímpares).py
@@ -1,16 +1,17 @@
 #Desafio 85: Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente. 
 
+#MODO FÁCIL - RESOLUÇÃO PESSOAL: USANDO O COMANDO SORTED
 
-#RESOLUÇÃO PESSOAL
+#MODO DIFICIL - RESOLUÇÃO PESSOAL: SEM USAR O COMANDO SORTED 
 valores_paridade = [[], []]
 
 for c in range(1, 8):
     valor = int(input(f'Digite o {c}° valor: '))
     if valor%2 == 0:
-        if len(valores_paridade[0]) == 0: #Primeiro Valor
+        if len(valores_paridade[0]) == 0: #adicionar o primeiro valor Par
             valores_paridade[0].append(valor)
 
-        elif valor>max(valores_paridade[0]): #Ultimo valor
+        elif valor>max(valores_paridade[0]): #Adicionar o maior valor Par
             valores_paridade[0].append(valor)
         
         else:
@@ -22,7 +23,7 @@ for c in range(1, 8):
                 pos += 1
     
     else:
-        if len(valores_paridade[1]) == 0: #adicionar o primeiro valor par
+        if len(valores_paridade[1]) == 0: #adicionar o primeiro valor Impar
             valores_paridade[1].append(valor)
 
         elif valor>max(valores_paridade[1]): #Adicionar o maior valor impar
@@ -38,4 +39,5 @@ for c in range(1, 8):
 
 print(f'Os valores pares digitados foram: {valores_paridade[0]} ')
 print(f'Os valores impares digitados foram: {valores_paridade[1]}')
+
 
