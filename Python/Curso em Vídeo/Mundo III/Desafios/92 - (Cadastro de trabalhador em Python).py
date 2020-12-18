c8dcf75ee8065cca13378fd31bc54ff0ef88fa06
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-12-18 19:58:31 -0300

    Desafio completo

--- Python/Curso em Vídeo/Mundo III/Desafios/92 - (Cadastro de trabalhador em Python).py
+++ Python/Curso em Vídeo/Mundo III/Desafios/92 - (Cadastro de trabalhador em Python).py
@@ -1,1 +1,23 @@
 #Desafio 92: Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionário se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
+
+#RESOLUÇÃO PESSOAL
+from datetime import datetime
+
+empregado = {'Nome': str(input('Digite seu nome: ')),
+             'Idade':datetime.now().year - int(input('Ano de nascimento: ')),
+             'Numero da carteira': int(input('Carteira de Trabalho(0 não tem): '))}
+
+if empregado['Numero da carteira'] != 0: 
+    empregado['Ano de contratação'] = int(input('Ano de contratação: '))
+    empregado['Salário'] = float(input('Qual o seu salário? '))
+    empregado['Aposentadoria'] =  empregado['Idade'] + ((empregado['Ano de contratação'] + 35) - datetime.now().year)
+    for k,v in empregado.items():
+        print(f'- {k} tem o valor {v}. ')
+
+else:
+    for k,v in empregado.items():
+        print(f'- {k} tem o valor {v}. ')
+
+
+
+
--- Python/Curso em Vídeo/Mundo III/Desafios/94 - (Unindo dicionários e listas).py
+++ Python/Curso em Vídeo/Mundo III/Desafios/94 - (Unindo dicionários e listas).py
@@ -4,3 +4,48 @@
 # C) Uma lista com todas as mulheres. 
 # D) Uma lista com todas as pessoas com idade acima da média. 
 
+
+#RESOLUÇÃO PESSOAL 
+soma_pessoas = 0
+soma_numerador = 0
+continuar = ''
+agrupamento_pessoas = []
+contador = 0
+while True: 
+    pessoas = {'Nome': str(input('Nome: ')),'Sexo': str(input('Sexo [M/F] ')).strip().upper()}
+    while pessoas['Sexo'] not in  'MF':
+        pessoas['Sexo'] = str(input('Resposta incorreta! Qual seu sexo? [M/F] ')).strip().upper()
+    pessoas['Idade'] = int(input('Idade: '))
+    agrupamento_pessoas.append(pessoas)
+    soma_pessoas = soma_pessoas + 1
+
+    continuar = str(input('Quer continuar? [S/N] ')).strip().upper()
+    while continuar not in 'S/N':
+        continuar = str(input('Resposta incorreta! Quer continuar? [S/N] ')).strip().upper()
+    if continuar == 'N':
+        break
+
+for c in range(0, len(agrupamento_pessoas)):
+    soma_numerador = soma_numerador + agrupamento_pessoas[c]['Idade'] 
+
+media = soma_numerador/soma_pessoas
+print(f'Foram cadastradas {soma_pessoas} pessoas.')
+print(f'A média de idade do grupo é de {media:.1f}.')
+
+print('As mulheres cadastradas foram:')
+for c in range(0, len(agrupamento_pessoas)):
+    if agrupamento_pessoas[c]['Sexo'] == 'F':
+        print(f'{agrupamento_pessoas[c]["Nome"]}...', end='')
+
+print('\nPessoas com a idade maior que a média: ')
+for c in range(0,len(agrupamento_pessoas)):
+    if agrupamento_pessoas[c]['Idade'] > media:
+        print(f'Nome = {agrupamento_pessoas[c]["Nome"]}; Sexo = {agrupamento_pessoas[c]["Sexo"]}; Idade = {agrupamento_pessoas[c]["Idade"]}')
+print(agrupamento_pessoas)
+
+
+
+
+
+
+
