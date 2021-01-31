831216abc243517a380052b826258109fde7e49f
Gustavo Gomes <gustavo.gomes012@gmail.com>
2021-01-31 17:33:06 -0300

    Desafio

---
+++ Python/Curso em Vídeo/Mundo III/Desafios/99 - (Função que descobre o maior).py
@@ -0,0 +1,1 @@
+#Desafio 99: Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
