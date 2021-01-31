13fa18475a9217860445390785ae6daebbec5aa6
Gustavo Gomes <gustavo.gomes012@gmail.com>
2021-01-31 15:08:13 -0300

    parte prática da aula teórica

---
+++ Python/Curso em Vídeo/Mundo III/Teoria/18 - (Funções 1).py
@@ -0,0 +1,13 @@
+
+def soma(a, b):
+    print(f'a = {a} e b = {b}.')
+    s = a + b
+    print(f'A soma a + b = {s}.')
+
+#Programa Principal
+
+soma(2, -4)
+soma(3, 6)
+soma(a=5,b=2)
+soma(a=2,b=25)
+
