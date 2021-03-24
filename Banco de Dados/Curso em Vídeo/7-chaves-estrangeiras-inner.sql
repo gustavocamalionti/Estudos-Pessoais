6eff87ff99012d8991a9417f254c032eda4aa9c0
Gustavo Gomes <gustavo.gomes012@gmail.com>
2021-03-23 21:51:17 -0300

    left and right outer join!!!

--- Banco de Dados/Curso em Vídeo/7-chaves-estrangeiras-inner.sql
+++ Banco de Dados/Curso em Vídeo/7-chaves-estrangeiras-inner.sql
@@ -32,9 +32,22 @@ from gafanhotos inner join cursos
 on cursos.idcurso = gafanhotos.cursopreferido
 order by gafanhotos.nome desc;
 
-/**/ 
+/*usando o parâmetro 'as' */ 
 select g.nome, g.cursopreferido, c.nome, c.ano
 from gafanhotos as g inner join cursos as c
 on c.idcurso = g.cursopreferido
 order by g.nome desc;
+
+/* left outer join & right outer join */
+select g.nome, g.cursopreferido, c.nome, c.ano
+from gafanhotos as g right outer join cursos as c
+on c.idcurso = g.cursopreferido
+order by g.nome desc;
+
+select g.nome, g.cursopreferido, c.nome, c.ano 
+from gafanhotos as g left outer join cursos as c
+on c.idcurso = g.cursopreferido;
+
+
+
 
