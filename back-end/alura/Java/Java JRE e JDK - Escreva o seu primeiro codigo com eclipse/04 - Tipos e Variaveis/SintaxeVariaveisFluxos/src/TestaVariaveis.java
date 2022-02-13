8a839dee487397c6358b06f268dc6e0f21b3512a
Gustavo Gomes <54083715+gustavocamalionti@users.noreply.github.com>
2022-02-13 14:02:24 -0300

    Trabalhando com a variável int

--- back-end/alura/Java/Java JRE e JDK - Escreva o seu primeiro codigo com eclipse/04 - Tipos e Variaveis/SintaxeVariaveisFluxos/src/TestaVariaveis.java
+++ back-end/alura/Java/Java JRE e JDK - Escreva o seu primeiro codigo com eclipse/04 - Tipos e Variaveis/SintaxeVariaveisFluxos/src/TestaVariaveis.java
@@ -1,5 +1,20 @@
 public class TestaVariaveis {
     public static void main(String[] args) {
+        System.out.println("Olá novo teste!");
 
+        //Java é uma linguagem Estaticamente tipada/ Fortemente Tipada.
+        //É necessário declarar todas as variáveis que serão usadas
+
+        int idade;
+        idade = 37;
+        System.out.println(idade);
+
+        idade = 30 + 10;
+        System.out.println(idade);
+
+        idade = (7 * 5) + 2;
+        System.out.println(idade);
+
+        System.out.println("a idade de Gustavo é de " + idade + " anos.");
     }
 }
