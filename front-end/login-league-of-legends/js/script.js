794b9d2e4a12b3c70f9b589d2b2c0c7162c05361
Gustavo Camalionti <gustavocamalionti@gmail.com>
2022-06-07 17:44:49 -0300

    Javascript | Dinâmica dos span completa

--- front-end/login-league-of-legends/js/script.js
+++ front-end/login-league-of-legends/js/script.js
@@ -1,9 +1,25 @@
 const inputs = document.querySelectorAll('.input');
 
-const handleFocus = function(event) {
-    console.log(event.target);
+const handleFocus = function({ target }) {
+   const span = target.previousElementSibling;
+   span.classList.add('span-active');
 }
+
+const handleFocusOut = function ({ target }) {
+    if (target.value == "") {
+        const span = target.previousElementSibling;
+        span.classList.remove('span-active');
+    }
+}
+
+//const handleFocus = function(event) {
+//    console.log(event.target);
+//}
 
 inputs.forEach(function(input) {
     input.addEventListener('focus', handleFocus);
+})
+
+inputs.forEach(function(input) {
+    input.addEventListener('focusout', handleFocusOut);
 })
